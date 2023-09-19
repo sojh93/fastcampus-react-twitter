89e34e355ce8211ea87952f8e371f86a2124ee6a
JeongHyeonSo <36619557+sojh93@users.noreply.github.com>
2023-09-19 17:22:48 +0900

    라우터 인덱스 정리

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,21 +1,31 @@
 import {Route, Routes, Navigate} from 'react-router-dom'
 import HomePage from 'pages/home';
+import PostListPage from 'pages/posts';
+import PostDetail from 'pages/posts/detail';
+import PostNew from 'pages/posts/new';
+import PostEdit from 'pages/posts/edit';
+import ProfilePage from 'pages/profile';
+import ProfileEdit from 'pages/profile/edit';
+import NotificationsPage from 'pages/notifications';
+import Search from 'pages/search';
+import LoginPage from 'pages/users/login';
+import SignupPage from 'pages/users/signup';
 
 export default function Router () {
     return (
-        <Routes>
-      <Route path='/' element={<HomePage/>}></Route>
-      <Route path='/posts' element={<h1>Post List Page</h1>}></Route>
-      <Route path='/posts/:id' element={<h1>Post Detail Page</h1>}></Route>
-      <Route path='/posts/new' element={<h1>Post New Page</h1>}></Route>
-      <Route path='/posts/edit/:id' element={<h1>Post Edit Page</h1>}></Route>
-      <Route path='/profile' element={<h1>Profile Page</h1>}></Route>
-      <Route path='/profile/edit' element={<h1>Profile Edit Page</h1>}></Route>
-      <Route path='/notifications' element={<h1>Notifications Page</h1>}></Route>
-      <Route path='/search' element={<h1>Search Page</h1>}></Route>
-      <Route path='/users/login' element={<h1>Login Page</h1>}></Route>
-      <Route path='/users/signup' element={<h1>Signup Page</h1>}></Route>
-      <Route path='*' element={<Navigate replace to="/" />}/>
-    </Routes>
+      <Routes>
+        <Route path='/' element={<HomePage/>}></Route>
+        <Route path='/posts' element={<PostListPage/>}></Route>
+        <Route path='/posts/:id' element={<PostDetail/>}></Route>
+        <Route path='/posts/new' element={<PostNew />}></Route>
+        <Route path='/posts/edit/:id' element={<PostEdit/>}></Route>
+        <Route path='/profile' element={<ProfilePage/>}></Route>
+        <Route path='/profile/edit' element={<ProfileEdit />}></Route>
+        <Route path='/notifications' element={<NotificationsPage />}></Route>
+        <Route path='/search' element={<Search />}></Route>
+        <Route path='/users/login' element={<LoginPage />}></Route>
+        <Route path='/users/signup' element={<SignupPage />}></Route>
+        <Route path='*' element={<Navigate replace to="/" />}/>
+      </Routes>
     )
 }
