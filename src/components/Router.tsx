2225f8d4c9c16b225f25dba00b84cce2e3f4baac
JeongHyeonSo <36619557+sojh93@users.noreply.github.com>
2023-09-19 16:47:14 +0900

    라우터 정리

---
+++ src/components/Router.tsx
@@ -0,0 +1,21 @@
+import {Route, Routes, Navigate} from 'react-router-dom'
+import HomePage from 'pages/home';
+
+export default function Router () {
+    return (
+        <Routes>
+      <Route path='/' element={<HomePage/>}></Route>
+      <Route path='/posts' element={<h1>Post List Page</h1>}></Route>
+      <Route path='/posts/:id' element={<h1>Post Detail Page</h1>}></Route>
+      <Route path='/posts/new' element={<h1>Post New Page</h1>}></Route>
+      <Route path='/posts/edit/:id' element={<h1>Post Edit Page</h1>}></Route>
+      <Route path='/profile' element={<h1>Profile Page</h1>}></Route>
+      <Route path='/profile/edit' element={<h1>Profile Edit Page</h1>}></Route>
+      <Route path='/notifications' element={<h1>Notifications Page</h1>}></Route>
+      <Route path='/search' element={<h1>Search Page</h1>}></Route>
+      <Route path='/users/login' element={<h1>Login Page</h1>}></Route>
+      <Route path='/users/signup' element={<h1>Signup Page</h1>}></Route>
+      <Route path='*' element={<Navigate replace to="/" />}/>
+    </Routes>
+    )
+}
